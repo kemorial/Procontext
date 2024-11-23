2c8f4ab67381a713b9d124915fc33f6d5d14b65d
FeldwebelDornan <gera.2002@list.ru>
2024-11-23 23:11:47 +0300

    new patch of run script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
 composer install
+cd docker-procontext
 docker-compose up -d --build
-docker exec -it procontext_mysql mysql -uroot -proot -c "CREATE DATABASE laravel"
 docker exec -it procontext_php bash -c "cd /var/www && php artisan migrate"
