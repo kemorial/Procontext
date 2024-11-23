6c2f54a26d7da294d433315462df546d11df825f
FeldwebelDornan <gera.2002@list.ru>
2024-11-23 23:53:06 +0300

    the LAST start script.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 composer install
 cd docker-procontext
 docker-compose up -d --build
-sleep 3
+sleep 8
 docker exec -it procontext_mysql mysql -uroot -proot -e "CREATE DATABASE laravel"
 docker exec -it procontext_php bash -c "cd /var/www && php artisan migrate"
