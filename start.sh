7f33722804570f1959b15e276d6bfd52305cde05
FeldwebelDornan <gera.2002@list.ru>
2024-11-23 23:38:12 +0300

    the last update of start script

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 composer install
 cd docker-procontext
 docker-compose up -d --build
+docker exec -it procontext_mysql mysql -uroot -proot -e "CREATE DATABASE laravel"
 docker exec -it procontext_php bash -c "cd /var/www && php artisan migrate"
