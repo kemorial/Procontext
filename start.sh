a0e25cda56575592897e21207e9230508b5f555d
FeldwebelDornan <gera.2002@list.ru>
2024-11-24 03:10:14 +0300

    cool razvert

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 composer install
 cd docker-procontext
 docker-compose up -d --build
-sleep 8
-docker exec -it procontext_mysql mysql -uroot -proot -e "CREATE DATABASE laravel"
 docker exec -it procontext_php bash -c "cd /var/www && php artisan migrate"
